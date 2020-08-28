1d9cc6b9c7e4171bf66610996e15cc2894a34023
koyamanX <ckoyama.1996@gmail.com>
2020-08-28 21:40:11 +0900

    improve baud rate

--- integration/uart_sender.h
+++ integration/uart_sender.h
@@ -1,6 +1,11 @@
 #ifndef UART_SENDER_H
 #define UART_SENDER_H
-#define TX_BAUD_RATE_CNT 5208
+#define TX_BAUD_RATE_9600 5208
+#define TX_BAUD_RATE_76800 651
+#define TX_BAUD_RATE_38400 1302
+#define TX_BAUD_RATE_19200 2604
+#define TX_BAUD_RATE_115200 434
+#define TX_BAUD_RATE_CNT TX_BAUD_RATE_19200
 
 declare uart_sender {
 	input adrs[10];
@@ -12,11 +17,12 @@ declare uart_sender {
 	func_out valid();
 	func_in reset();
 	func_out done();
-	func_out int_req();
 }
 
 struct uart_tx_stat_t {
-	x[6];	
+	x[4];	
+	full[1];
+	empty[1];
 	busy[1];
 	en[1];
 };
