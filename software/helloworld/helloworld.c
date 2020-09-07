e6d556a2a66e2fc86991ab8da10ec32c1b8735b6
koyamanX <ckoyama.1996@gmail.com>
2020-09-08 02:08:51 +0900

    mod to use libgloss and newlib

--- software/helloworld/helloworld.c
+++ software/helloworld/helloworld.c
@@ -1,31 +1,8 @@
-void wait_uart(void) {
-	while((*((volatile unsigned int *) 0x40000004) & 0x6) != 0x4)
-		;
-}
-void uart_putchar(int ch) {
-	while((*((volatile unsigned int *) 0x40000004)) & 0x8)
-		;
-	*((volatile unsigned int *) 0x40000000) = ch;
-	if(!(*((volatile unsigned int *)0x40000004) & 0x1))
-		*((volatile unsigned int *) 0x40000004) = 1;
-}
-void uart_puts(char *str) {
-	while(*str) {
-		uart_putchar(*str);
-		str++;
-	}
-	uart_putchar('\n');
-}
-int main(void) {
-	int i;
+#include <unistd.h>
 
-	uart_puts("hello,world!");
-	wait_uart();
+int main(void) {
+	write(STDOUT_FILENO, "hello,world!\n", 13);
 
 	return 0;
 }
 
-extern void (* _exit)(int);
-void exit(int stat) {
-	_exit(stat);
-}
