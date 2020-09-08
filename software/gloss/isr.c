69d86f60e46fc6d479a38d048f2ee368e2135ae8
koyamanX <ckoyama.1996@gmail.com>
2020-09-09 02:49:07 +0900

    add lines to update mepc in exception handler

--- software/gloss/isr.c
+++ software/gloss/isr.c
@@ -1,5 +1,19 @@
 
 __attribute__ ((interrupt ("machine"))) void exception_handler(void) {
+	register unsigned int a0 asm("a0");
+	register unsigned int a1 asm("a1");
+	register unsigned int a2 asm("a2");
+	register unsigned int a3 asm("a3");
+	register unsigned int a4 asm("a4");
+	register unsigned int a5 asm("a5");
+	register unsigned int a6 asm("a6");
+	register unsigned int a7 asm("a7");
+	register unsigned int mepc;
+
+	asm volatile("csrr %0, mepc" : "=r"(mepc):);
+	mepc += 4;
+	asm volatile("csrw mepc, %0" :: "r"(mepc));	
+
 	return ;
 }
 __attribute__ ((interrupt ("machine"))) void trap_handler(void) {
