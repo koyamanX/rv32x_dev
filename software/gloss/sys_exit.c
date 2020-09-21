01d260bf43dade09e0c1696edfd8638cd605f4f2
koyamanX <ckoyama.1996@gmail.com>
2020-09-22 04:45:49 +0900

    delete some files

--- software/gloss/sys_exit.c
+++
@@ -1,5 +0,0 @@
-void sys_exit(int status) {
-
-	while(1)
-		asm volatile("nop");
-}
--- software/gloss/sys_misc.c
+++
@@ -1,3 +0,0 @@
-/* environ is defined as external variable at unistd.h */
-char *__env[1] = {0};
-char **environ = __env;
