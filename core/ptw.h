5aca7c1d32c13685cec63c21fc3dcc34f3c09e01
koyamanX <ckoyama.1996@gmail.com>
2020-11-01 07:08:57 +0900

    reorder signal declearations

--- core/ptw.h
+++ core/ptw.h
@@ -5,12 +5,12 @@ declare ptw {
 	input satp[32];
 	input virtual_address[32];
 	func_in walk(virtual_address);
+	func_out page_fault();
+	output page_table_entry[32];
+	func_out pte_valid(page_table_entry);
 	output mem_adrs[34];
 	input mem_rdata[32];
 	func_out mem_read(mem_adrs);
 	func_in mem_valid;
-	func_out page_fault();
-	output page_table_entry[32];
-	func_out pte_valid(page_table_entry);
 }
 #endif
