66379bdb4ce7170bfeafe111ec28d1331f5e08be
koyamanX <ckoyama.1996@gmail.com>
2020-11-08 10:40:54 +0900

    fix some behavior in PTW module and add test

--- core/ptw.h
+++ core/ptw.h
@@ -3,11 +3,12 @@
 declare ptw {
 	func_in reset();
 	input satp[32];
-	input virtual_address[32];
-	func_in walk(virtual_address);
+	input vadrs[32];
+	func_in walk(vadrs);
 	func_out page_fault();
-	output page_table_entry[32];
-	func_out pte_valid(page_table_entry);
+	output pte[32];
+	func_out pte_valid(pte);
+
 	output mem_adrs[34];
 	input mem_rdata[32];
 	func_out mem_read(mem_adrs);
