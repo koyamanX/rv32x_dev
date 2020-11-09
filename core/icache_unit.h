3148d2a6a015a1b072ca6e72f0d64e57465d1baf
koyamanX <ckoyama.1996@gmail.com>
2020-11-09 18:43:10 +0900

    make them simpler and readable

--- core/icache_unit.h
+++ core/icache_unit.h
@@ -6,7 +6,6 @@
 #define VALID 1'b1
 #define INVALID 1'b0
 
-/* If hits, rdata and valid signals available with no delay */
 declare icache_unit {
 	func_in reset();
 	/* CPU <-> Cache Unit */
@@ -16,25 +15,34 @@ declare icache_unit {
 	/* if need to cancel operation, then deassert these signals before valid 
 		however contents of cache will be updated even with cancelation */
 	func_in read(adrs);				/* must be asserted until valid */
-	func_out valid;	/* hit */
+	func_out valid;					/* hit */
 
+	/* invalidate all cache entries */
 	func_in invalidate;
 	func_out invalidate_done;
 
+	/* satp from CPU core for instruction page access */
 	input satp_mode;
 	input satp_asid[9];
 	input satp_ppn[22];
 
+	/* flush all TLB entries, no writeback */
 	func_in tlb_flush();
 	func_out tlb_flush_done();
 
+	/* instruction faults, no PMA check on instruction fetch
+		however, pma is checked on page_table_walk, and instruction_access_fault
+		is raised if page_table_walk violates PMAs
+	*/
 	func_out instruction_access_fault();
 	func_out instruction_page_fault();
 
+	/* Cache Unit <-> Page table walker Interface */
+	/* satp for page table walk to page table walker */
 	output satp[32];
 	output vadrs[32];
-	input pte[32];
 	func_out walk(satp, vadrs);
+	input pte[32];
 	func_in pte_valid();
 	func_in page_fault();
 
@@ -44,5 +52,4 @@ declare icache_unit {
 	func_out mem_read(mem_adrs);
 	func_in mem_valid;
 }
-
 #endif
