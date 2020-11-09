682d6ff665a6023377430a0008f7d8e1cca9a5d8
koyamanX <ckoyama.1996@gmail.com>
2020-11-09 19:59:33 +0900

    make them simpler and readable

--- core/dcache_unit.h
+++ core/dcache_unit.h
@@ -23,14 +23,35 @@ declare dcache_unit {
 	func_in cache_flush;
 	func_out cache_flush_done;
 
+	input satp_mode;
+	input satp_asid[9];
+	input satp_ppn[22];
+	input mstatus_mxr;
+	input mstatus_mprv;
+	input priv_mode[2];
+	input sstatus_sum;
+
+	/* flush all TLB entries, no writeback */
+	func_in tlb_flush();
+	func_out tlb_flush_done();
+
+	func_out store_amo_access_fault();
+	func_out load_access_fault();
+
+	/* Cache Unit <-> uTLB interfaces */
+	input tlb_adrs[32];
+	func_in read_tlb(tlb_adrs);
+	output pte[32];
+	func_out pte_valid();
+	func_out tlb_read_page_fault();	
+
+	/* Cache Unit <-> Memory Interface */
 	output pma_adrs[32];
 	func_out pma_check(pma_adrs);
 	func_in pma_empty();
 	func_in pma_cacheable();
-	func_out store_amo_access_fault();
-	func_out load_access_fault();
 
-	/* Cache Unit <-> Memory Interface */
+	/* use 32 bit physical address even with sv32 enabled */
 	output mem_adrs[32];
 	input mem_rdata[128];
 	output mem_wdata[128];
@@ -38,12 +59,5 @@ declare dcache_unit {
 	func_out mem_read(mem_adrs, mem_size);
 	func_out mem_write(mem_adrs, mem_size, mem_wdata);
 	func_in mem_valid;
-
-	input satp_mode;
-	input satp_asid[9];
-	input satp_ppn[22];
-	input mstatus_mprv;
-	input mstatus_mxr;
 }
-
 #endif
