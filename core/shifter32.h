4ff7462516d47095c8479b821433410a2ad9b1c2
koyamanX <ckoyama.1996@gmail.com>
2021-01-11 01:46:58 +0900

    rename signals'

--- core/shifter32.h
+++ core/shifter32.h
@@ -1,15 +1,17 @@
 #ifndef SHIFTER32_H
 #define SHIFTER32_H
+
 #define SHIFTER_SL 1'b0
 #define SHIFTER_SR 1'b1
 #define SHIFTER_LOGICAL 1'b0
 #define SHIFTER_ARITHMETIC 1'b1
+
 declare shifter32 {
 	input a[32];
 	input b[5];
-	input c;
+	input op;
 	input arithmetic; /* 1 for arithmetic, 0 for logical */
 	output q[32];
-	func_in exe(a, b, c, arithmetic) : q;
+	func_in exe(a, b, op, arithmetic) : q;
 }
 #endif
