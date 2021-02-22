d6a7d11428c018433108c58e640645f7c23da63b
koyamanX <ckoyama.1996@gmail.com>
2021-02-22 22:28:05 +0900

    rename bus signals

--- integration/bootrom.h
+++ integration/bootrom.h
@@ -1,10 +1,12 @@
 #ifndef BOOTROM_H
 #define BOOTROM_H
+
 declare bootrom {
-	input port1_radrs[12];
-	output port1_rdata[32];
-	func_in port1_read(port1_radrs);
-	func_out port1_valid();
+	input addr[12];
+	output rdata[32];
+	func_in read(addr);
+	func_out ready();
 	func_in reset();
 }
+
 #endif
