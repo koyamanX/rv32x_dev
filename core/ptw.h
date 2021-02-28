8a5596db047d192e39c7a00cb348239b38e9e7c5
koyamanX <ckoyama.1996@gmail.com>
2021-02-28 21:26:08 +0900

    rename signals

--- core/ptw.h
+++ core/ptw.h
@@ -3,16 +3,16 @@
 declare ptw {
 	func_in reset();
 	input satp[32];
-	input vadrs[32];
-	func_in walk(satp, vadrs);
+	input vaddr[32];
+	func_in walk(satp, vaddr);
 	func_out page_fault();
 	output pte[32];
 	output pte_levels[2];
 	func_out pte_valid(pte, pte_levels);
 
-	output mem_adrs[34];
+	output mem_addr[34];
 	input mem_rdata[32];
-	func_out mem_read(mem_adrs);
-	func_in mem_valid;
+	func_out mem_read(mem_addr);
+	func_in mem_ready;
 }
 #endif
