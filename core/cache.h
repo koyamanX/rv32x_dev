8e5f025d1cf870553dcc8c3ff0b6230427816011
koyamanX <ckoyama.1996@gmail.com>
2021-03-12 03:39:14 +0900

    rename signals

--- core/cache.h
+++ core/cache.h
@@ -10,15 +10,14 @@
 #define CACHE_READ	1'b0
 
 declare cache {
-	/* CPU <-> Cache Unit */
-	input adrs[32];
+	input addr[32];
 	output rdata[32];
 	input wdata[32];
 	input byteen[3];
 	func_in reset();
-	func_in read(adrs, byteen);				/* must be asserted until valid arrives */
-	func_in write(adrs, byteen, wdata);		/* must be asserted until valid arrives */
-	func_out valid;							/* hit */
+	func_in read(addr, byteen);
+	func_in write(addr, byteen, wdata);
+	func_out valid;
 
 	func_in cache_flush;
 	func_out cache_flush_done;
@@ -27,18 +26,18 @@ declare cache {
 	func_out load_access_fault();
 
 	/* Cache Unit <-> Memory Interface */
-	output pma_adrs[32];
-	func_out pma_check(pma_adrs);
+	output pma_addr[32];
+	func_out pma_check(pma_addr);
 	func_in pma_empty();
 	func_in pma_cacheable();
 
 	/* use 32 bit physical address even with sv32 enabled */
-	output mem_adrs[32];
+	output mem_addr[32];
 	input mem_rdata[128];
 	output mem_wdata[128];
 	output mem_size[3];
-	func_out mem_read(mem_adrs, mem_size);
-	func_out mem_write(mem_adrs, mem_size, mem_wdata);
+	func_out mem_read(mem_addr, mem_size);
+	func_out mem_write(mem_addr, mem_size, mem_wdata);
 	func_in mem_valid;
 }
 #endif
