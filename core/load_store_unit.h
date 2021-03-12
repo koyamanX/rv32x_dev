c8ad90aa765fec7038f37225472d7bd8e9cd7fe6
koyamanX <ckoyama.1996@gmail.com>
2021-03-13 05:42:47 +0900

    add P-BUS I/F

--- core/load_store_unit.h
+++ core/load_store_unit.h
@@ -15,13 +15,12 @@ struct reservation_t {
 };
 
 declare load_store_unit {
-	/* CPU <-> Cache Unit */
+	/* CPU operations (Virtual Address) */
+	func_in reset();
 	input addr[32];
+	input byteen[3];
 	output load_data[32];
 	input store_data[32];
-	input byteen[3];
-	func_in reset();
-	/* Operations */
 	func_in load(addr, byteen);					/* must be asserted until valid arrives */
 	func_in store(addr, byteen, store_data);	/* must be asserted until valid arrives */
 	func_in load_reserved();					/* This signal is asserted alongside with load */
@@ -38,27 +37,24 @@ declare load_store_unit {
 
 	func_in cache_flush;
 	func_out cache_flush_done;
+	func_in tlb_flush();
+	func_out tlb_flush_done();
 
+	/* CSRs(read-only) */
 	input satp_mode;
 	input satp_asid[9];
 	input satp_ppn[22];
 	input mstatus_mxr;
 	input priv_mode[2];
 	input sstatus_sum;
-
-	/* flush all TLB entries, no writeback */
-	func_in tlb_flush();
-	func_out tlb_flush_done();
-
+	
+	/* Exceptions to CPU */
 	func_out store_amo_page_fault();
 	func_out store_amo_access_fault();
 	func_out load_page_fault();
 	func_out load_access_fault();
 
-	func_in bus_load_access_fault();
-	func_in bus_store_amo_access_fault();
-
-	/* Cache Unit <-> uTLB interfaces */
+	/* uTLB I/F */
 	input utlb_addr[32];
 	func_in read_utlb(utlb_addr);
 	output utlb_pte[32];
@@ -67,7 +63,7 @@ declare load_store_unit {
 	func_out utlb_page_fault();
 	func_out utlb_access_fault();
 
-	/* Cache Unit <-> Memory Interface */
+	/* PMAs */
 	output pma_addr[32];
 	func_out pma_check(pma_addr);
 	func_in pma_writable();
@@ -79,7 +75,8 @@ declare load_store_unit {
 	func_in pma_cacheable();
 	func_in pma_empty();
 
-	/* use 32 bit physical address even with sv32 enabled */
+	/* Pysical Address */
+	/* D-BUS(Cacheable, may support burst access with maximum word width of 128 bits) */
 	output mem_addr[32];
 	input mem_rdata[128];
 	output mem_wdata[128];
@@ -87,6 +84,22 @@ declare load_store_unit {
 	func_out mem_read(mem_addr, mem_size);
 	func_out mem_write(mem_addr, mem_size, mem_wdata);
 	func_in mem_valid;
+	func_in bus_load_access_fault();
+	func_in bus_store_amo_access_fault();
+
+	/* Pysical Address */
+	/* P-BUS(Noncacheable, no burst access, maximum word width of 32 bits, may support byte, halfword access) */
+	output pbus_addr[32];
+	input pbus_rdata[32];
+	output pbus_wdata[32];
+	output pbus_size[3];
+	func_out pbus_read(pbus_addr, pbus_size);
+	func_out pbus_write(pbus_addr, pbus_size, pbus_wdata);
+	/* P-BUS status */
+	func_in pbus_ready();
+	func_in pbus_load_access_fault();
+	func_in pbus_store_access_fault();
+
 #ifdef DEBUG
 	output tohost[32];
 	func_out sim_done(tohost);
