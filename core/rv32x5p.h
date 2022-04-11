6cdf6b44a0fe5224d25995d229ef52bd90b73960
twinkletia <vamosidemochumjetze215@gmail.com>
2022-04-12 08:03:29 +0900

    interrupt bug fix

--- core/rv32x5p.h
+++ core/rv32x5p.h
@@ -1,29 +1,30 @@
 #ifndef RV32X5P_H
 #define RV32X5P_H
-declare rv32x5p {
-	input resetvector[32];                      /* Initial PC value on reset */
-	func_in reset(resetvector);                 /* Reset processor */
+declare rv32x5p
+{
+	input resetvector[32];						/* Initial PC value on reset */
+	func_in reset(resetvector);					/* Reset processor */
 	input ialign[2];							/* ialign (32==2'b00, 16==2'b10) */
-	input inst[32];                             /* Irnstruction read on imem_read, only valid during imem_ready */
-	input rdata[32];                            /* Data read on dmem_read, only valid during dmem_ready */
-	output wdata[32];                           /* Write data to dmem */
-	output daddr[32];                           /* Address to dmem */
-	output iaddr[32];                           /* Address to imem */
-	output ibyteen[3];                          /* Byte enable of imem */
-	output dbyteen[3];                          /* Byte enable of dmem */
-	output cradrs[12];                          /* CSR address for read */
-	output cwadrs[12];                          /* CSR address for write */
-	input  crdata[32];                          /* Data read  */
-	output cwdata[32];                          /* Data to be written */
-	func_in dmem_ready;                         /* Indication of completation of current dmem transfer */
-	func_in imem_ready;                         /* Indication of completation of current imem transfer */
-	func_out imem_read(iaddr, ibyteen);         /* When data is ready on 'inst', imem_valid is asserted */
-	func_out dmem_read(daddr, dbyteen);         /* When data is ready on 'rdata', dmem_ready is asserted */
+	input inst[32];								/* Irnstruction read on imem_read, only valid during imem_ready */
+	input rdata[32];							/* Data read on dmem_read, only valid during dmem_ready */
+	output wdata[32];							/* Write data to dmem */
+	output daddr[32];							/* Address to dmem */
+	output iaddr[32];							/* Address to imem */
+	output ibyteen[3];							/* Byte enable of imem */
+	output dbyteen[3];							/* Byte enable of dmem */
+	output cradrs[12];							/* CSR address for read */
+	output cwadrs[12];							/* CSR address for write */
+	input crdata[32];							/* Data read  */
+	output cwdata[32];							/* Data to be written */
+	func_in dmem_ready;							/* Indication of completation of current dmem transfer */
+	func_in imem_ready;							/* Indication of completation of current imem transfer */
+	func_out imem_read(iaddr, ibyteen);			/* When data is ready on 'inst', imem_valid is asserted */
+	func_out dmem_read(daddr, dbyteen);			/* When data is ready on 'rdata', dmem_ready is asserted */
 	func_out dmem_write(daddr, dbyteen, wdata); /* When data is written, dmem_ready is asserted */
 	func_out csr_write_uimm();
-	func_out csr_read(cradrs) : crdata;         /* CSR read */
-	func_out csr_write(cwadrs, cwdata);         /* CSR write */
-	
+	func_out csr_read(cradrs) : crdata; /* CSR read */
+	func_out csr_write(cwadrs, cwdata); /* CSR write */
+
 	func_out load_reserved();
 	func_out store_conditional();
 	output amo_src[32];
@@ -33,21 +34,21 @@ declare rv32x5p {
 	output amo_rl;
 	output amo_width[3];
 	func_out amo_exe(amo_src, amo_addr, amo_funct5, amo_aq, amo_rl, amo_width, daddr);
-	func_out fencei();							/* Indicating fence.i is issued */
-	func_out fence();							/* Indicating fence is issued */
+	func_out fencei(); /* Indicating fence.i is issued */
+	func_out fence();  /* Indicating fence is issued */
 	output asid[9];
 	output vaddr[32];
-	func_out sfence_vma(vaddr, asid);			/* Indicating sfence_vma is issued */
-	func_out ecall();							/* Indicating ecall is issued */
-	func_out ebreak();							/* Indicating ebreak is issued */
-	func_out mret();							/* Indicating mret is issued */
-	func_out sret();							/* Indicating sret is issued */
-	func_out uret();							/* Indicating uret is issued */
-	func_out wfi();								/* indicating Wait-For-Interrupt is issued */
+	func_out sfence_vma(vaddr, asid); /* Indicating sfence_vma is issued */
+	func_out ecall();				  /* Indicating ecall is issued */
+	func_out ebreak();				  /* Indicating ebreak is issued */
+	func_out mret();				  /* Indicating mret is issued */
+	func_out sret();				  /* Indicating sret is issued */
+	func_out uret();				  /* Indicating uret is issued */
+	func_out wfi();					  /* indicating Wait-For-Interrupt is issued */
 
 	/* Trap */
-	input vector[32];							/* New PC on trap */
-	output epc[32];								/* Exception PC */
+	input vector[32]; /* New PC on trap */
+	output epc[32];	  /* Exception PC */
 	func_in trap(vector);
 	func_in flush_ifetch;
 	func_in flush_decode;
@@ -69,8 +70,9 @@ declare rv32x5p {
 	func_in get_pc_decode() : epc;
 	func_in get_pc_execute() : epc;
 	func_in get_pc_memory() : epc;
+	func_in get_pc_trap() : epc;
 	/* Get internal instruction at the stage */
-	output einst[32];		/* Exception instruction */
+	output einst[32]; /* Exception instruction */
 	func_in get_inst_execute() : einst;
 	func_in get_inst_memory() : einst;
 
