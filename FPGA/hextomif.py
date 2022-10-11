87b60fdeea5d33c3b2ee1d3cfbe97723de3d0491
twinkletia <vamosidemochumjetze215@gmail.com>
2022-10-11 18:06:37 +0900

    fix fpga makefile

--- FPGA/hextomif.py
+++ FPGA/hextomif.py
@@ -3,9 +3,10 @@
 import sys
 
 
-def hextomif(input, output):
+def hextomif(input, target):
     array = []
     addr = 0x0
+    mem = []
     with open(input, 'r') as f:
         l = f.readlines()
         for i in [i for i in l if '@' not in i]:
@@ -13,20 +14,44 @@ def hextomif(input, output):
             for j in a:
                 array.append(j)
         # print(array)
-    buf = f'DEPTH=@;\nWIDTH=8;\nADDRESS_RADIX=HEX;\nDATA_RADIX=HEX;\nCONTENT BEGIN\n\n{0:04x} :'
-    for i in array:
-        if (addr != 0) and ((addr % 16 == 0)):
-            buf += f';\n{addr:04x} :'
-        buf += f' {i}'
-        addr += 1
-    buf += ';\nEND;'
-    # print(buf)
-    buf = buf.replace('@', str(addr))
-    with open(output, 'w') as f:
-        f.write(buf)
+    if target == "bootrom":
+        cnt = 0
+        buf = f'DEPTH=@;\nWIDTH=32;\nADDRESS_RADIX=HEX;\nDATA_RADIX=HEX;\nCONTENT BEGIN\n\n{0:04x} :'
+        for i in array:
+            if (addr != 0) and ((addr % 4 == 0) and cnt % 4 == 0):
+                buf += f';\n{addr:04x} :'
+
+            if ((cnt % 4) == 0):
+                buf += f' {i}'
+                addr += 1
+            else:
+                buf += f'{i}'
+            cnt += 1
+        buf += ';\nEND;'
+        # print(buf)
+        buf = buf.replace('@', str(addr))
+        with open("bootrom.mif", 'w') as f:
+            f.write(buf)
+    elif target == "memory":
+        for num in range(4):
+            mem.append(array[num::4])
+
+        for num in range(4):
+            addr = 0
+            buf = f'DEPTH=@;\nWIDTH=8;\nADDRESS_RADIX=HEX;\nDATA_RADIX=HEX;\nCONTENT BEGIN\n\n{0:04x} :'
+            for i in mem[num]:
+                if (addr != 0) and ((addr % 16 == 0)):
+                    buf += f';\n{addr:04x} :'
+                buf += f' {i}'
+                addr += 1
+            buf += ';\nEND;'
+            # print(buf)
+            buf = buf.replace('@', str(addr))
+            with open("memory_"+str(num)+".mif", 'w') as f:
+                f.write(buf)
 
 
 if __name__ == "__main__":
     input = sys.argv[1]
-    output = sys.argv[2]
-    hextomif(input, output)
+    target = sys.argv[2]  # "bootrom" or "memory"
+    hextomif(input, target)
--- integration/tcm.h
+++ integration/tcm.h
@@ -1,12 +1,19 @@
 #ifndef TCM_H
 #define TCM_H
-declare tcm {
+#define IMEM 2'b01
+#define DMEM 2'b10
+declare tcm
+{
+	param_str filename;
 	input addr[32];
 	input wdata[32];
 	input byteen[3];
+	input type[2];
 	output rdata[32];
-	func_in read(addr, byteen);
+	func_in reset();
+	func_in read(addr, byteen, type);
 	func_in write(addr, byteen, wdata);
-	func_out ready();
+	func_out iready();
+	func_out dready();
 }
 #endif
