b7380f90f03124772210e1ec77cca7f9fc0d9bf2
twinkletia <vamosidemochumjetze215@gmail.com>
2023-09-29 17:25:04 +0900

    change container setting

--- run.sh
+++ run.sh
@@ -5,8 +5,6 @@ docker run -it --rm \
     -e LOCAL_GID=$(id -g $USER) \
     -e USER=$USER \
     -e DISPLAY=$DISPLAY \
-    -v $XAUTHORITY:/tmp/.XAuthority \
-    -e XAUTHORITY=/tmp/.XAuthority \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     --privileged \
     -v /sys:/sys:ro \
@@ -18,3 +16,7 @@ docker run -it --rm \
 	-v $(pwd)/simulation:/root/simulation \
     -v $(pwd)/FPGA:/root/FPGA \
     rv32x_dev:v1.0 $1
+
+
+#    -v $XAUTHORITY:/tmp/.XAuthority \
+#    -e XAUTHORITY=/tmp/.XAuthority \
