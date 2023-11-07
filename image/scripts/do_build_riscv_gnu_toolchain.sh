d71bfedbfb89b724788cf2cc5adc44c161a551d6
twinkletia <vamosidemochumjetze215@gmail.com>
2023-11-07 09:01:20 +0900

    replace development tools

--- image/scripts/do_build_riscv_gnu_toolchain.sh
+++ image/scripts/do_build_riscv_gnu_toolchain.sh
@@ -1,23 +1,42 @@
 #!/bin/bash
 set -ex
+CTNG_VERSION=1.26.0
+BINUTILS_VERSION=2.40
 
-git clone -b 2023.06.09 https://github.com/riscv/riscv-gnu-toolchain
+mkdir build
+pushd build &> /dev/null
+	wget http://crosstool-ng.org/download/crosstool-ng/crosstool-ng-$CTNG_VERSION.tar.bz2
+	tar -xvf crosstool-ng-$CTNG_VERSION.tar.bz2
+	wget https://ftp.gnu.org/gnu/binutils/binutils-$BINUTILS_VERSION.tar.xz
+	tar -xvf binutils-$BINUTILS_VERSION.tar.xz
+	git clone https://github.com/twinkletia/linux --depth=1 -b rv32xSoC
 
-pushd riscv-gnu-toolchain &> /dev/null
-	git submodule update --init --progress --depth=1 binutils dejagnu gcc gdb glibc newlib
-	mkdir build
-	pushd build &> /dev/null
-		../configure --prefix=/opt/riscv --with-arch=rv32ima --with-abi=ilp32
-		make linux -j $(nproc)
-		make newlib -j $(nproc)
-	popd &> /dev/null
-	mkdir binutils/build
-	pushd binutils/build &> /dev/null
-		../configure --prefix=/opt/riscv --target=riscv32-elflittle --enable-commonbfdlib --disable-gdb --enable-install-libbfd 
+	mkdir binutils-$BINUTILS_VERSION/build
+	pushd binutils-$BINUTILS_VERSION/build &> /dev/null
+		../configure --prefix=/opt/riscv --target=riscv32-elflittle --enable-commonbfdlib --disable-gdb --enable-install-libbfd --enable-install-libiberty
 		make -j $(nproc)
 		make install-bfd -j $(nproc)
 		make install-opcodes -j $(nproc)
+		make install-libsframe -j $(nproc)
+		make install-libiberty -j $(nproc)
 	popd &> /dev/null
-popd &> /dev/null
-rm -rf riscv-gnu-toolchain
 
+	pushd crosstool-ng-$CTNG_VERSION &> /dev/null
+		./bootstrap
+		mkdir build
+		pushd build &> /dev/null
+		../configure
+		make
+		make install
+		popd &> /dev/null
+	popd &> /dev/null
+
+	ct-ng defconfig DEFCONFIG=../riscv_cross_newlib_defconfig
+	ct-ng build
+	ct-ng defconfig DEFCONFIG=../riscv_cross_linux_defconfig
+	ct-ng build
+	export PATH=/opt/riscv/bin:$PATH
+	ct-ng defconfig DEFCONFIG=../riscv_native_linux_defconfig
+	ct-ng build
+popd &> /dev/null
+rm -rf build
